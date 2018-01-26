54db571cb24cc2ae6bedb513c118c39707eb0f15
Danny Ben Shitrit <dannyb@telesofia.com>
2018-01-26 10:46:14 +0000

    fix the broken ruby 2.5.0 getc method
    as it seems, ruby 2.5.0 has some issue with .getc, so we are switching to using .gets instead.
    see: https://goo.gl/uaSQMS

---
+++ example/README.md
@@ -0,0 +1,12 @@
+Example
+==================================================
+
+This folder contains an example spec file.
+
+In order to see all behaviors of RSpec Fixtures, we need to invalidate
+the fixtures, so for convenience, you can run:
+
+    $ run example
+
+from the root folder of this repository, to reset fixtures and run the
+spec file interactively.
--- example/sample_spec.rb
+++ example/sample_spec.rb
@@ -1,10 +1,46 @@
 require 'rspec'
 require 'rspec_fixtures'
+require "fileutils"
 
-describe 'output_fixture' do
-  subject { puts "hello world" }
+RSpec.configure { |c| c.fixtures_path = 'example/fixtures' }
+
+describe 'interactivity' do
+  subject { "shiny fixture" }
+
+  context 'when a fixture does not exist' do
+    before do
+      file = 'example/fixtures/interactive1'
+      FileUtils.rm file if File.exist? file
+    end
+
+    it 'asks the user to approve the new content' do
+      expect(subject).to match_fixture('interactive1')
+    end
+  end
+
+  context 'when the fixture exists, but different' do
+    before do
+      File.write 'example/fixtures/interactive2', 'old, wrinkled fixture'
+    end
+
+    it 'shows the old and new versions to the user' do
+      expect(subject).to match_fixture('interactive2')
+    end
+  end
+end
+
+describe 'match_fixture' do
+  subject { "this fixture is a match" }
 
   it "works" do
-    expect{ subject }.to output_fixture("tmp/sample")
+    expect(subject).to match_fixture("match_fixture")
+  end
+end
+
+describe 'output_fixture' do
+  subject { puts "this fixture is an output" }
+
+  it "works" do
+    expect{ subject }.to output_fixture("output_fixture")
   end
 end
