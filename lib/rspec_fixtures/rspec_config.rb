819cfa2ff737f13473ef4ee23c2f27949f776bdc
Danny Ben Shitrit <db@dannyben.com>
2020-01-30 21:12:45 +0000

    test with GitHub actions
    and disable interactivity when GITHUB_ACTIONS is true

--- lib/rspec_fixtures/rspec_config.rb
+++ lib/rspec_fixtures/rspec_config.rb
@@ -3,7 +3,7 @@ if defined? RSpec
   RSpec.configure do |config|
     config.include RSpecFixtures::Matchers
     config.add_setting :fixtures_path, default: File.expand_path('spec/fixtures')
-    config.add_setting :interactive_fixtures, default: !ENV['CI']
+    config.add_setting :interactive_fixtures, default: (!ENV['CI'] and !ENV['GITHUB_ACTIONS'])
     config.add_setting :auto_approve, default: ENV['AUTO_APPROVE']
     config.add_setting :strip_ansi_escape, default: false
     config.add_setting :before_approval, default: nil
